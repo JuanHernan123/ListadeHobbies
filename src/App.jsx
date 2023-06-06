05154380b2f39ea6533b5d1c84b5676831c01de6
Juan Hernan <128071729+JuanHernan123@users.noreply.github.com>
2023-06-06 17:46:02 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const name= "Juan Alejandro Luis Hernandez";
+  return (
+    <div>
+      <h3>Lista de Hobbies de {name}</h3>
+      <ul>
+        <li>Tocar la guitarra</li>
+        <li>Salir con amigos</li>
+        <li>Escuchar musica</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
